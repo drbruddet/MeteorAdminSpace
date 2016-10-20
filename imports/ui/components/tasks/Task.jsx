9238352abd3d12d81b0591088d526f97c5a4c1fa
Guillaume Kolly <guillaume.kolly@gmail.com>
2016-10-20 23:17:08 +0200

    add information popover to tasks

--- imports/ui/components/tasks/Task.jsx
+++ imports/ui/components/tasks/Task.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Checkbox, Button, Glyphicon, Label } from 'react-bootstrap'
+import { Checkbox, ButtonGroup, Button, Glyphicon, Label, OverlayTrigger, Popover } from 'react-bootstrap'
 
 import './Task.styl'
 
@@ -27,8 +27,19 @@ class Task extends Component {
 		}
 	}
 
+	dateFormate(time) {
+ 		return ((moment().unix() - moment(time).unix()) < 3600) ? 
+ 			moment(time).fromNow() : moment(time).format("DD / MM / YYYY HH:mm");
+	}
+
 	render() {
 		const taskClassName = this.props.task.checked ? 'success' : '';
+
+		const popoverHoverFocus = (
+			<Popover id="popover-trigger-hover-focus" title="Informations">
+				<strong>Created: </strong>{this.dateFormate(this.props.task.createdAt)}
+			</Popover>
+		);
 
 		return (
 			<tr className={taskClassName}>
@@ -47,10 +58,17 @@ class Task extends Component {
 				<td className="vert-align">
 					{this.props.task.text}
 				</td>
-				<td className="vert-align col-sm-2 col-md-2 col-lg-1 text-right">
-					<Button bsStyle="danger" className="btn-sm" onClick={() => this.deleteThisTask()}>
-						<Glyphicon glyph="glyphicon glyphicon-remove" />
-					</Button>
+				<td className="vert-align col-sm-2 col-md-2 col-lg-2 text-right">
+					<ButtonGroup>
+						<Button className="btn-sm">
+    						<OverlayTrigger trigger={['hover', 'focus']} placement="top" overlay={popoverHoverFocus}>
+								<Glyphicon glyph="glyphicon glyphicon-info-sign" />
+							</OverlayTrigger>
+						</Button>
+    					<Button bsStyle="danger" className="btn-sm" onClick={() => this.deleteThisTask()}>
+							<Glyphicon glyph="glyphicon glyphicon-remove" />
+						</Button>
+  					</ButtonGroup>
 				</td>
 			</tr>
 		);
